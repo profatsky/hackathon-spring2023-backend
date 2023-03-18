09c2714f84a13f007f45a53db231044bdba762c6
Mikhail Profatsky <mixail.rostov04@gmail.com>
2023-03-18 19:58:14 +0300

    added some changes for models fields, registered models in admin panel

--- src/connection_requests/admin.py
+++ src/connection_requests/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ConnectionRequest, Client, Status, StatusGroup, Service, AdditionalSalesChannel, TVPTestType, \
+    TVPPresence, Installer, AgentInstaller, IPTVTariffPlan, TariffPlan
+
+
+@admin.register(ConnectionRequest)
+class ConnectionRequestAdmin(admin.ModelAdmin):
+    list_display = ('number', 'status',)
+    list_filter = ('status',)
+    raw_id_fields = ('client', 'status', 'service', 'additional_sales_channel', 'completed_by', 'TVP_test_type',
+                     'TVP_presence', 'installer', 'agent_installer', 'started_by', 'IPTV_tariff_plan')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('title', 'INN')
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('title', 'group')
+    list_filter = ('group',)
+
+
+@admin.register(StatusGroup)
+class StatusGroupAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(AdditionalSalesChannel)
+class AdditionalSalesChannelAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(TVPTestType)
+class TVPTestTypeAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(TVPPresence)
+class TVPPresenceAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(Installer)
+class InstallerAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'surname')
+
+
+@admin.register(AgentInstaller)
+class AgentInstallerAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(IPTVTariffPlan)
+class IPTVTariffPlanAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(TariffPlan)
+class TariffPlanAdmin(admin.ModelAdmin):
+    list_display = ('title',)
--- src/connection_requests/models.py
+++ src/connection_requests/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class ConnectionRequest(models.Model):
     number = models.PositiveIntegerField(verbose_name='Номер заявки')
@@ -11,63 +13,76 @@ class ConnectionRequest(models.Model):
     service = models.ForeignKey('Service', related_name='connection_requests', on_delete=models.PROTECT,
                                 verbose_name='Услуга')
     additional_sales_channel = models.ForeignKey('AdditionalSalesChannel', related_name='connection_requests',
-                                                 on_delete=models.PROTECT, null=True, verbose_name='Доп.канал продаж')
+                                                 on_delete=models.PROTECT, blank=True, null=True,
+                                                 verbose_name='Доп.канал продаж')
     reg_date = models.DateTimeField(verbose_name='Дата регистрации')
-    reg_date_for_request = models.DateTimeField(verbose_name='Дата регистрации под заявки')
-    reg_date_brigade_for_TVP = models.DateTimeField(verbose_name='Регистрация наряда на ТВП')
-    # completed_by = models.ForeignKey('Operator', related_name='connection_requests', on_delete=models.PROTECT,
-    #                                  verbose_name='Оператор завершивший заявку')
-    rejection_date = models.DateTimeField(verbose_name='Дата отклонения под заявки')
+    reg_date_for_request = models.DateTimeField(null=True, verbose_name='Дата регистрации под заявки')
+    reg_date_brigade_for_TVP = models.DateTimeField(null=True, verbose_name='Регистрация наряда на ТВП')
+    completed_by = models.ForeignKey(User, related_name='completed_connection_requests', on_delete=models.PROTECT,
+                                     blank=True, null=True, verbose_name='Оператор завершивший заявку')
+    rejection_date = models.DateTimeField(null=True, verbose_name='Дата отклонения под заявки')
     TVP_test_type = models.ForeignKey('TVPTestType', related_name='connection_requests', on_delete=models.PROTECT,
-                                      verbose_name='Тип проверки ТВП')
+                                      null=True, verbose_name='Тип проверки ТВП')
     TVP_presence = models.ForeignKey('TVPPresence', related_name='connection_requests', on_delete=models.PROTECT,
-                                     verbose_name='Наличие ТВП')
-    completion_TVP_date = models.DateTimeField(verbose_name='Завершение проверки ТВП')
-    TVP_check_duration = models.CharField(max_length=8, verbose_name='Длительность проверки ТВП')
-    agreed_connection_date = models.DateTimeField(verbose_name='Согласованная дата подключения')
-    installer = models.ForeignKey('Installer', related_name='connection_requests', null=True, on_delete=models.PROTECT,
-                                  verbose_name='Инсталлятор')
-    agent_installer = models.ForeignKey('AgentInstaller', related_name='connection_requests', null=True,
-                                        on_delete=models.PROTECT, verbose_name='Агент-инсталлятор')
+                                     null=True, verbose_name='Наличие ТВП')
+    completion_TVP_date = models.DateTimeField(null=True, verbose_name='Завершение проверки ТВП')
+    TVP_check_duration = models.TimeField(null=True, verbose_name='Длительность проверки ТВП')
+    agreed_connection_date = models.DateTimeField(blank=True, null=True, verbose_name='Согласованная дата подключения')
+    installer = models.ForeignKey('Installer', related_name='connection_requests', on_delete=models.PROTECT, blank=True,
+                                  null=True, verbose_name='Инсталлятор')
+    agent_installer = models.ForeignKey('AgentInstaller', related_name='connection_requests', on_delete=models.PROTECT,
+                                        blank=True, null=True, verbose_name='Агент-инсталлятор')
     comment_TVP_ShPD = models.CharField(max_length=250, blank=True, verbose_name='Комментарий ТВП ШПД')
-    # started_by = models.ForeignKey('Operator', related_name='connection_requests', on_delete=models.PROTECT,
-    #                              verbose_name='Оператор заводивший заявку')
-    id_agreement_KURS = models.CharField(max_length=250, verbose_name='ID Договор КУРС')
-    note = models.CharField(max_length=250, unique=True, verbose_name='Примечание')
-    contact_phone_number = models.PositiveIntegerField(verbose_name='Контактный телефон')
+    started_by = models.ForeignKey(User, related_name='started_connection_requests', on_delete=models.PROTECT,
+                                   blank=True, null=True, verbose_name='Оператор заводивший заявку')
+    id_agreement_KURS = models.CharField(max_length=250, blank=True, verbose_name='ID Договор КУРС')
+    note = models.CharField(max_length=250, blank=True, verbose_name='Примечание')
+    contact_phone_number = models.PositiveIntegerField(blank=True, null=True, verbose_name='Контактный телефон')
     contact_person = models.CharField(max_length=200, blank=True, verbose_name='Контактное лицо')
-    TP_ShPD_price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Стоимость ТП (ШПД)')
-    TP_IPTV_price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Стоимость ТП (IPTV)')
+    TP_ShPD_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True,
+                                        verbose_name='Стоимость ТП (ШПД)')
+    TP_IPTV_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True,
+                                        verbose_name='Стоимость ТП (IPTV)')
     contract_created_by_fio = models.CharField(max_length=250, blank=True,
                                                verbose_name='ФИО сотрудника создавшего договор')
     IPTV_tariff_plan = models.ForeignKey('IPTVTariffPlan', related_name='connection_requests', on_delete=models.PROTECT,
-                                         verbose_name='Тарифный план IPTV')
-    access_card_number = models.PositiveIntegerField(verbose_name='Номер карты доступа')
-    IPTV_access_card_number = models.PositiveIntegerField(verbose_name='Номер карты доступа IPTV')
+                                         blank=True, null=True, verbose_name='Тарифный план IPTV')
+    access_card_number = models.PositiveIntegerField(blank=True, null=True, verbose_name='Номер карты доступа')
+    IPTV_access_card_number = models.PositiveIntegerField(blank=True, null=True,
+                                                          verbose_name='Номер карты доступа IPTV')
     tariff_plan = models.ForeignKey('TariffPlan', related_name='connection_requests', on_delete=models.PROTECT,
-                                    verbose_name='Тарифный план')
-    client_number_SUS = models.PositiveIntegerField(verbose_name='№ клиентский СУС')
-    sending_date_APTV = models.DateTimeField(verbose_name='Дата отправки на АПТВ')
-    finishing_date_APTV_planned = models.DateTimeField(verbose_name='Дата окончания АПТВ планируемая')
-    finishing_date_APTV_actual = models.DateTimeField(verbose_name='Дата окончания АПТВ фактическая')
-    APTV_duration = models.CharField(max_length=8, verbose_name='Длительность этапа АПТВ')
-    sending_date_DO = models.DateTimeField(verbose_name='Дата отправки на ДО')
-    finishing_date_DO_planned = models.DateTimeField(verbose_name='Дата окончания ДО планируемая')
-    finishing_date_DO_actual = models.DateTimeField(verbose_name='Дата окончания ДО фактическая')
-    DO_duration = models.CharField(max_length=8, verbose_name='Длительность этапа ДО')
+                                    blank=True, null=True, verbose_name='Тарифный план')
+    client_number_SUS = models.PositiveIntegerField(blank=True, null=True, verbose_name='№ клиентский СУС')
+    sending_date_APTV = models.DateTimeField(blank=True, null=True, verbose_name='Дата отправки на АПТВ')
+    finishing_date_APTV_planned = models.DateTimeField(blank=True, null=True,
+                                                       verbose_name='Дата окончания АПТВ планируемая')
+    finishing_date_APTV_actual = models.DateTimeField(blank=True, null=True,
+                                                      verbose_name='Дата окончания АПТВ фактическая')
+    APTV_duration = models.TimeField(blank=True, null=True, verbose_name='Длительность этапа АПТВ')
+    sending_date_DO = models.DateTimeField(blank=True, null=True, verbose_name='Дата отправки на ДО')
+    finishing_date_DO_planned = models.DateTimeField(blank=True, null=True,
+                                                     verbose_name='Дата окончания ДО планируемая')
+    finishing_date_DO_actual = models.DateTimeField(blank=True, null=True, verbose_name='Дата окончания ДО фактическая')
+    DO_duration = models.TimeField(blank=True, null=True, verbose_name='Длительность этапа ДО')
 
     class Meta:
         verbose_name = 'Заявка на подключение'
         verbose_name_plural = 'Заявки на подключение'
 
+    def __str__(self):
+        return f"Заявка №{self.number}"
+
 
 class Client(models.Model):
     title = models.CharField(max_length=250, verbose_name='Название клиента')
-    INN = models.PositiveIntegerField(verbose_name='ИНН')
+    INN = models.PositiveBigIntegerField(verbose_name='ИНН')
 
     class Meta:
         verbose_name = 'Клиент'
         verbose_name_plural = 'Клиенты'
+
+    def __str__(self):
+        return self.title
 
 
 class Status(models.Model):
@@ -77,6 +92,10 @@ class Status(models.Model):
     class Meta:
         verbose_name = 'Статус заявки'
         verbose_name_plural = 'Статусы заявок'
+        ordering = ('group', 'title')
+
+    def __str__(self):
+        return self.title
 
 
 class StatusGroup(models.Model):
@@ -86,6 +105,9 @@ class StatusGroup(models.Model):
         verbose_name = 'Группа статуса'
         verbose_name_plural = 'Группы статусов'
 
+    def __str__(self):
+        return self.title
+
 
 class Service(models.Model):
     title = models.CharField(max_length=50, unique=True, verbose_name='Название')
@@ -93,6 +115,9 @@ class Service(models.Model):
     class Meta:
         verbose_name = 'Услуга'
         verbose_name_plural = 'Услуги'
+
+    def __str__(self):
+        return self.title
 
 
 class AdditionalSalesChannel(models.Model):
@@ -102,6 +127,9 @@ class AdditionalSalesChannel(models.Model):
         verbose_name = 'Доп.канал продажи'
         verbose_name_plural = 'Доп.каналы продажи'
 
+    def __str__(self):
+        return self.title
+
 
 class TVPTestType(models.Model):
     title = models.CharField(max_length=10, unique=True, verbose_name='Название')
@@ -109,6 +137,9 @@ class TVPTestType(models.Model):
     class Meta:
         verbose_name = 'Тип проверки ТВП'
         verbose_name_plural = 'Типы проверок ТВП'
+
+    def __str__(self):
+        return self.title
 
 
 class TVPPresence(models.Model):
@@ -118,15 +149,21 @@ class TVPPresence(models.Model):
         verbose_name = 'Наличие ТВП'
         verbose_name_plural = 'Наличие ТВП'
 
+    def __str__(self):
+        return self.title
+
 
 class Installer(models.Model):
-    fist_name = models.CharField(max_length=50, verbose_name='Имя')
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
     last_name = models.CharField(max_length=50, verbose_name='Фамилия')
     surname = models.CharField(max_length=50, verbose_name='Отчество')
 
     class Meta:
         verbose_name = 'Инсталлятор'
         verbose_name_plural = 'Инсталляторы'
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} {self.surname}"
 
 
 class AgentInstaller(models.Model):
@@ -136,6 +173,9 @@ class AgentInstaller(models.Model):
         verbose_name = 'Агент-инсталлятор'
         verbose_name_plural = 'Агенты-инсталляторы'
 
+    def __str__(self):
+        return self.title
+
 
 class IPTVTariffPlan(models.Model):
     title = models.CharField(max_length=100, unique=True, verbose_name='Название')
@@ -144,6 +184,9 @@ class IPTVTariffPlan(models.Model):
         verbose_name = 'Тарифный план IPTV'
         verbose_name_plural = 'Тарифные планы IPTV'
 
+    def __str__(self):
+        return self.title
+
 
 class TariffPlan(models.Model):
     title = models.CharField(max_length=100, unique=True, verbose_name='Название')
@@ -151,3 +194,16 @@ class TariffPlan(models.Model):
     class Meta:
         verbose_name = 'Тарифный план'
         verbose_name_plural = 'Тарифные планы'
+
+    def __str__(self):
+        return self.title
+
+#
+# class TestModel(models.Model):
+#     title = models.CharField(max_length=250)
+#     description = models.TextField()
+#     link = models.ForeignKey('LinkModel', related_name='tests', on_delete=models.PROTECT)
+#
+#
+# class LinkModel(models.Model):
+#     title = models.CharField(max_length=250)
